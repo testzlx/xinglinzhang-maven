6b39113372990e4f336a401416615be0fd0afee9
zhanglinxing <zhanglinxing@meituan.com>
2019-04-23 16:11:25 +0800

    不考虑分包粘包的情型

--- src/main/java/com/sankuai/netty/TimeClient.java
+++ src/main/java/com/sankuai/netty/TimeClient.java
@@ -1,138 +1,60 @@
 package com.sankuai.netty;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
-import java.util.Iterator;
-import java.util.Set;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class TimeClient {
-
     public static void main(String[] args) throws  Exception{
         int port = 8080;
-       // bio(port);
-        nio(port);
-    }
-
-    private static void nio(int port) {
-            new Thread(new TimeClientHandle(port),"client").start();
-    }
-
-    private static void bio(int port) throws  Exception{
-        Socket socket = new Socket("127.0.0.1",port);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-        out.println("query time");
-        System.out.println("send query time");
-        String resp = in.readLine();
-        System.out.println("resp: "+resp);
-        Thread.sleep(4000);
-    }
-
-     static class  TimeClientHandle implements  Runnable{
-        private Selector selector;
-        private SocketChannel socketChannel;
-        private String host = "127.0.0.1";
-        private Integer port;
-        private volatile boolean stop = false;
-
-        public TimeClientHandle(int port){
-            try {
-                this.port = port;
-                selector = Selector.open();
-                socketChannel = SocketChannel.open();
-                socketChannel.configureBlocking(false);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-
-        }
-        @Override
-        public void run() {
-            try{
-                doConnect();
-            }catch (Exception e){
-                System.out.println("exception:"+e);
-            }
-            while(!stop){
-                try {
-                    selector.select(1000);
-                    Set<SelectionKey> selectionKeySet = selector.selectedKeys();
-                    Iterator<SelectionKey> it = selectionKeySet.iterator();
-                    SelectionKey key = null;
-                    while (it.hasNext()) {
-                        key = it.next();
-                        it.remove();
-                        handInput(key);
-                    }
-                }catch (Exception e){
-                    System.out.println("e:"+e);
-                    if(selector != null){
-                        try {
-                            selector.close();
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        try{
+            Bootstrap b = new Bootstrap();
+            b.group(bossGroup).channel(NioSocketChannel.class).
+                    option(ChannelOption.TCP_NODELAY,true).
+                    handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel socketChannel) throws Exception{
+                            socketChannel.pipeline().addLast(new TimeClientHandle());
                         }
-                    }
-                }
-            }
+                    });
+            ChannelFuture f = b.connect("127.0.0.1",port).sync();
+            f.channel().closeFuture().sync();
+        }finally {
 
-        }
-
-         private void handInput(SelectionKey key) throws  IOException {
-             if(key.isValid()){
-                 SocketChannel sc =(SocketChannel)key.channel();
-                 if(key.isConnectable()){
-                     if(sc.finishConnect()){
-                         sc.register(selector,SelectionKey.OP_READ);
-                         doWrite(sc);
-                     }else{
-
-                     }
-                 }
-                 if(key.isReadable()) {
-                     ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                     int count = sc.read(byteBuffer);
-                     if (count > 0) {
-                         byteBuffer.flip();
-                         byte[] bytes = new byte[byteBuffer.remaining()];
-                         byteBuffer.get(bytes);
-                         String body = new String(bytes, "UTF-8");
-                         System.out.println("body:" + body);
-                         stop = true;
-                     } else if (count < 0) {
-                         key.cancel();
-                         sc.close();
-                     } else {
-
-                     }
-                 }
-             }
-         }
-
-         private void doConnect() throws  Exception {
-            if(socketChannel.connect(new InetSocketAddress(host,port))){
-                socketChannel.register(selector, SelectionKey.OP_READ);
-                doWrite(socketChannel);
-            }else{
-                socketChannel.register(selector,SelectionKey.OP_CONNECT);
-            }
-        }
-
-        private void doWrite(SocketChannel socketChannel) throws IOException {
-            byte[] bytes = "query time".getBytes();
-            ByteBuffer byteBuffer = ByteBuffer.allocate(bytes.length);
-            byteBuffer.put(bytes);
-            byteBuffer.flip();
-            socketChannel.write(byteBuffer);
-            System.out.println("client send msg");
         }
     }
+
+   static class  TimeClientHandle extends ChannelHandlerAdapter {
+        private final ByteBuf byteBuf ;
+
+        public TimeClientHandle(){
+            byte[] msg = "query time".getBytes();
+            byteBuf = Unpooled.buffer(msg.length);
+            byteBuf.writeBytes(msg);
+        }
+
+       @Override
+       public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+          ctx.close();
+       }
+
+       @Override
+       public void channelActive(ChannelHandlerContext ctx) throws Exception {
+          ctx.writeAndFlush(byteBuf);
+       }
+
+       @Override
+       public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+           ByteBuf byteBuf = (ByteBuf) msg;
+           byte[] req = new byte[byteBuf.readableBytes()];
+           byteBuf.readBytes(req);
+           String body = new String(req,"UTF-8");
+           System.out.println("client is "+body);
+       }
+   }
 }
--- src/main/java/com/sankuai/netty/TimeServer.java
+++ src/main/java/com/sankuai/netty/TimeServer.java
@@ -1,176 +1,72 @@
 package com.sankuai.netty;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public class TimeServer {
 
-    static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(8,12,120l, TimeUnit.SECONDS,
-            new ArrayBlockingQueue<Runnable>(20));
-
-    public static void main(String[] args) throws Exception {
-        int port = 8080;
-        //bio(port);
-        nio(port);
-    }
-
-    private static void nio(int port) {
-        MultiplexerTimeService multiplexerTimeService = new MultiplexerTimeService(port);
-        new Thread(multiplexerTimeService,"server").start();
-    }
-
-    static class MultiplexerTimeService implements  Runnable{
-        private Selector selector;
-        private ServerSocketChannel serverSocketChannel;
-        private volatile  boolean stop = false;
-        public MultiplexerTimeService(int port){
-            try{
-                selector = Selector.open();
-                serverSocketChannel = ServerSocketChannel.open();
-                serverSocketChannel.configureBlocking(false);
-                serverSocketChannel.socket().bind(new InetSocketAddress(port),1024);
-                serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            }catch (Exception e){
-                System.out.println("MultiplexerTimeService construct"+ e);
-            }
-        }
-
-        @Override
-        public void run() {
-            while (!stop){
-                try{
-                    selector.select(1000);
-                    Set<SelectionKey> selectionKeySet = selector.selectedKeys();
-                    Iterator<SelectionKey> it = selectionKeySet.iterator();
-                    SelectionKey key = null;
-                    while(it.hasNext()){
-                        key = it.next();
-                        it.remove();
-                        handInput(key);
-                    }
-                }catch (Exception e){
-                    System.out.println("run "+ e);
-                    stop = true;
-                    if(selector != null){
-                        try {
-                            selector.close();
-                        } catch (IOException e1) {
-                            e1.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }
-
-        private void handInput(SelectionKey key) throws  IOException {
-            if(key.isValid()){
-                if(key.isAcceptable()){
-                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel)key.channel();
-                    SocketChannel sc = serverSocketChannel.accept();
-                    sc.configureBlocking(false);
-                    sc.register(selector,SelectionKey.OP_READ);
-                }
-                if(key.isReadable()){
-                    SocketChannel sc =(SocketChannel)key.channel();
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                    int count = sc.read(byteBuffer);
-                    if(count > 0) {
-                        byteBuffer.flip();
-                        byte[] bytes = new byte[byteBuffer.remaining()];
-                        byteBuffer.get(bytes);
-                        String body = new String(bytes,"UTF-8");
-                        System.out.println("body:" + body);
-                        String currentTime = "query time".equals(body) ? (new Date()).toString() : "bad order";
-                        doWrite(currentTime,sc);
-                    }else if(count<0){
-                        key.cancel();
-                        sc.close();
-                    }else{
-
-                    }
-                }else if(key.isWritable()){
-                    System.out.println("isWritable occur!");
-                }
-            }
-        }
-
-        private void doWrite(String currentTime,SocketChannel sc) throws  IOException {
-            byte[] bytes = currentTime.getBytes();
-            ByteBuffer byteBuffer = ByteBuffer.allocate(bytes.length);
-            byteBuffer.put(bytes);
-            byteBuffer.flip();
-            int writeCount =  sc.write(byteBuffer);
-            System.out.println("doWrite!"+currentTime+" writeCount: "+writeCount);
-        }
-    }
-
-
-    private static void  bio(int port) throws IOException{
-        ServerSocket serverSocket = null;
+    public void bind(int port) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
         try{
-            serverSocket = new ServerSocket(port);
-            Socket socket =null;
-            while (true){
-                socket = serverSocket.accept();
-                //一个请求新起一个线程
-                //new Thread(new TimeServerHandler(socket)).start();
-                //线程池 ，伪异步
-                threadPoolExecutor.submit(new TimeServerHandler(socket));
-            }
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup,workGroup).
+                    channel(NioServerSocketChannel.class).
+                    option(ChannelOption.SO_BACKLOG,1024).
+                    childHandler(new ChildChanelHandle());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
         }finally {
-            if(serverSocket != null){
-                serverSocket.close();
-            }
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
 
+    }
+
+    class ChildChanelHandle extends ChannelInitializer<SocketChannel>{
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandle());
         }
     }
 
-    static class TimeServerHandler implements  Runnable{
-        private Socket socket;
-        TimeServerHandler(Socket socket){
-            this.socket = socket;
+    class TimeServerHandle extends ChannelHandlerAdapter{
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            //super.exceptionCaught(ctx, cause);
+            ctx.close();
         }
-
 
         @Override
-        public void run()  {
-            BufferedReader in ;
-            PrintWriter out ;
-            try{
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                out = new PrintWriter(socket.getOutputStream(),true);
-                String currentTime,body;
-                while(true){
-                    body = in.readLine();
-                    System.out.println("body: "+body+ " "+new Date());
-                    if(body == null){
-                        break;
-                    }
-                    currentTime = "query time".equals(body) ? (new Date()).toString() : "bad order";
-                    out.println(currentTime);
-                }
-            }catch (Exception e){
-                System.out.println("TimeServerHandler err:"+e);
-            }
-            finally {
-
-            }
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            byte[] req = new byte[byteBuf.readableBytes()];
+            byteBuf.readBytes(req);
+            String body = new String(req,"UTF-8");
+            System.out.println("server : "+body);
+            String currentTime = "query time".equals(body) ? (new Date()).toString() : "bad order";
+            ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+            ctx.write(resp);
         }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+           // super.channelReadComplete(ctx);
+            ctx.flush();
+        }
+    }
+
+    public static void main(String[] args)  throws Exception{
+        int port = 8080;
+        new TimeServer().bind(port);
     }
 }
